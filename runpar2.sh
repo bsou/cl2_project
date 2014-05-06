86be9b6bf62c102fd818bb28b858e6ad7dfd58cc
Sudha Rao <sudharao@Sudhas-MacBook-Air.local>
2014-05-06 18:47:24 -0400

    changed runpar2.sh

--- runpar2.sh
+++ runpar2.sh
@@ -1,24 +1,24 @@
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 1 --end 523 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 524 --end 1046 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 1047 --end 1569 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 1570 --end 2092 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 2093 --end 2615 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 2616 --end 3138 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 3139 --end 3661 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 3662 --end 4184 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 4185 --end 4707 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 4708 --end 5230 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 5231 --end 5753 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 5754 --end 6276 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 6277 --end 6799 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 6800 --end 7322 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 7323 --end 7845 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 7846 --end 8368 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 8369 --end 8891 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 8892 --end 9414 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 9415 --end 9937 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 9938 --end 10460 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 10461 --end 10983 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 10984 --end 11506 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 11507 --end 12029 --outputfolder processed_unlabeledstatusfiles/
-python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 12030 --end 12539 --outputfolder processed_unlabeledstatusfiles/
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 1 --end 523 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 524 --end 1046 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 1047 --end 1569 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 1570 --end 2092 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 2093 --end 2615 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 2616 --end 3138 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 3139 --end 3661 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 3662 --end 4184 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 4185 --end 4707 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 4708 --end 5230 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 5231 --end 5753 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 5754 --end 6276 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 6277 --end 6799 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 6800 --end 7322 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 7323 --end 7845 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 7846 --end 8368 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 8369 --end 8891 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 8892 --end 9414 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 9415 --end 9937 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 9938 --end 10460 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 10461 --end 10983 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 10984 --end 11506 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 11507 --end 12029 --outputfolder processed_unlabeledstatusfiles/ &
+nohup python preprocessing.py --folderpath newunlabeledstatusfiles/ --start 12030 --end 12539 --outputfolder processed_unlabeledstatusfiles/ &
