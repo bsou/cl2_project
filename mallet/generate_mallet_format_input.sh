9678cdc34af50ca35dc4483a35d5e9f70e3ecb49
Sudha Rao <sudharao@Sudhas-MacBook-Air.local>
2014-05-11 13:44:22 -0400

    Update to mallet docs

--- mallet/generate_mallet_format_input.sh
+++ mallet/generate_mallet_format_input.sh
@@ -1,1 +1,1 @@
-~/Documents/Fall_2013/CL1/Project/mallet-2.0.7/bin/mallet import-dir --input dataset/newlabeledstatusfiles --output dataset/mallet/newlabeledstatusfiles.mallet --keep-sequence --remove-stopwords
+~/Documents/Fall_2013/CL1/Project/mallet-2.0.7/bin/mallet import-dir --input mallet_document_input --output 50/topic-input.mallet --keep-sequence --remove-stopwords
